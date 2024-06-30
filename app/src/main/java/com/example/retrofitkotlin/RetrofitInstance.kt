ff0f8dbb94246a64c2e954cf2237a37d5773c6d8
Purna chandra <purna90857@gmail.com>
2024-06-30 22:39:40 +0530

    initial change

---
+++ app/src/main/java/com/example/retrofitkotlin/RetrofitInstance.kt
@@ -0,0 +1,21 @@
+package com.example.retrofitkotlin
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance {
+
+    companion object {
+        val mainURL = "https://jsonplaceholder.typicode.com/"
+
+        fun getRetrofitInstance(): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(mainURL)
+                .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+                .build()
+        }
+    }
+
+}
